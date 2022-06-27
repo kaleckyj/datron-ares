b98a64124c525b32e6e0cd40c8a4f6faa9bc01f6
kaleckyj <honza.kalecky@centrum.cz>
2022-06-27 11:13:11 +0200

    working search
    basic GET method

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -35,41 +35,22 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 
     public function searchFormSucceeded(Form $form, $data): void
 	{
-        date_default_timezone_set('Europe/Prague');
-        $date = date("Y-m-d") . 'T' . date("H:i:s");
+        $url = "https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_std.cgi?ico=" . $data->ico;
+        $xml = simplexml_load_file($url);
 
-        try {
-            $client = new \SoapClient('https://wwwinfo.mfcr.cz/ares/xml_doc/wsdl/basic.wsdl');
+        $res = new \STDClass();
 
-            $xml = new \SimpleXMLElement("<Ares_dotazy></Ares_dotazy>");
-
-            $xml->addAttribute('dotaz_datum_cas', $date);
-            $xml->addAttribute('dotaz_pocet', '1');
-            $xml->addAttribute('dotaz_typ', 'Basic');
-            $xml->addAttribute('vystup_format', 'XML');
-            $xml->addAttribute('validation_XSLT', 'http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_answer/v_1.0.0/ares_answer.xsl');
-            $xml->addAttribute('user_mail', 'honza.kalecky@centrum.cz');
-            $xml->addAttribute('answerNamespaceRequired', 'http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_answer_basic/v_1.0.3');
-            $xml->addAttribute('Id', 'Ares_dotaz');
-
-            $query = $xml->addChild('Dotaz');
-            $query->addChild('Pomocne_ID', '1');
-            $query->addChild('ICO', $data->ico);
-
-            $xml->asXML("filename.xml");
-
-            //$str = '<are:Ares_dotazy xmlns:are="http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_request_orrg/v_1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_request_orrg/v_1.0.0 http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_request_orrg/v_1.0.0/ares_request_orrg.xsd" dotaz_datum_cas="' . $date .'" dotaz_pocet="1" dotaz_typ="Basic" vystup_format="XML" validation_XSLT="http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_answer/v_1.0.0/ares_answer.xsl" user_mail="honza.kalecky@centrum.cz" answerNamespaceRequired="http://wwwinfo.mfcr.cz/ares/xml_doc/schemas/ares/ares_answer_basic/v_1.0.3" Id="Ares_dotaz"><Dotaz><Pomocne_ID>1</Pomocne_ID><ICO>00025500</ICO></Dotaz></are:Ares_dotazy>';
-
-            //$response = $client->GetXmlFile($str);
-            $response = $client->GetXmlFile($xml->asXML());
-
-            var_dump($response);
-
-        } catch (Exception $e) {
-            echo $e->getMessage();
+        if ($xml->xpath("//are:Pocet_zaznamu")[0] == 0) $res->err = "IČO nebylo nalezeno";
+        else {
+            $res->ico = $data->ico;
+            $res->name = $xml->xpath("//are:Obchodni_firma")[0];
+            $res->city = $xml->xpath("//dtt:Nazev_obce")[0];
+            $res->street = $xml->xpath("//dtt:Nazev_ulice")[0];
+            $res->snum = $xml->xpath("//dtt:Cislo_domovni")[0];
+            $res->psc = $xml->xpath("//dtt:PSC")[0];
         }
 
-        //$this->forward('this', $res);
+        $this->forward('this', $res);
 	}
 
 }
